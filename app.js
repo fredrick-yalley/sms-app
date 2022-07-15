04590747ac4107da8f635b1d81d264f078bb9f27
fredrick <yalleyfred@gmail.com>
2022-07-15 20:53:56 +0000

    updated app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const messageRouter = require('./routes/messageRoute');
 const viewRouter = require('./routes/viewsRoute');
 const templateRouter = require('./routes/templateRoute');
+const compression = require("compression");
 const app = express();
 
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use('css', express.static(__dirname + 'public/css'));
 app.use('js', express.static(__dirname + 'public/js'));
 app.use('img', express.static(__dirname + 'public/img'));
 
+app.use(compression());
 
 // set views 
 app.set('views', path.join(__dirname, 'views'));
